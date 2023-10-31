f48d738d03938496a521eb4c186b9d37813b4567
KajalMule <mulekajal411@gmail.com>
2023-10-31 12:24:23 +0530

    Add New File

---
+++ src/Just_Example.java
@@ -0,0 +1,10 @@
+
+public class Just_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This file is taken to learn GIT");
+
+	}
+
+}

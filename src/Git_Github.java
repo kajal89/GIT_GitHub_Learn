7eea2595157605d89e165efc576bbdef6577889f
KajalMule <mulekajal411@gmail.com>
2023-10-31 09:52:38 +0530

    Inital Commit

---
+++ src/Git_Github.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Git_Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Two numbers :");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("Addition of two number = " + c);
+		sc.close();
+	}
+
+}
